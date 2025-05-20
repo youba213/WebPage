1a0e3937fd076d963144a80168f647391d1cbe13
FERHOUNE Youba <feryouba0@gmail.com>
2025-05-20 17:53:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ base_html = """
         }}
         .login-container {{
             width: 380px;
-            padding: 100px 40px; 
+            padding: 200px 40px; 
             background-color: white;
             border-radius: 12px;
             box-shadow: 0 4px 20px rgba(0, 0, 0, 0.1);
